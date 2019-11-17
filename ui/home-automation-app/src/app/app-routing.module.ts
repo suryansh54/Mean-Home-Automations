f5731b7fe98f9d136a2c9f53866e17c129ad7f69
Suryansh <suryanshsrivastava9616@gmail.com>
2019-11-17 21:58:47 +0530

    Basic UI Changes Adding Routes

--- ui/home-automation-app/src/app/app-routing.module.ts
+++ ui/home-automation-app/src/app/app-routing.module.ts
@@ -1,8 +1,40 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+// Featured Components
+import { DashboardComponent } from './featured-component/dashboard/dashboard.component';
+import { AddDeviceComponent } from './featured-component/add-device/add-device.component';
+import { AboutComponent } from './featured-component/about/about.component';
+import { DevicesComponent } from './featured-component/devices/devices.component';
+import { UpdateComponent } from './featured-component/update/update.component';
+import { ProfileComponent } from './featured-component/profile/profile.component';
+
+const routes: Routes = [
+  {
+    path:'' ,
+    component: DashboardComponent
+  }, 
+  {
+    path:'add-device' ,
+    component: AddDeviceComponent
+  }, 
+  {
+    path:'about' ,
+    component: AboutComponent
+  },
+  {
+    path:'devices' ,
+    component: DevicesComponent
+  },{
+    path:'firmware-update' ,
+    component: UpdateComponent
+  },
+  {
+    path:'profile' ,
+    component: ProfileComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ui/home-automation-app/src/app/app.module.ts
+++ ui/home-automation-app/src/app/app.module.ts
@@ -3,14 +3,43 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './fragments/header/header.component';
+import { FooterComponent } from './fragments/footer/footer.component';
+import { SidebarComponent } from './fragments/sidebar/sidebar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Angular Material imports
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+
+// Featured Components
+import { DashboardComponent } from './featured-component/dashboard/dashboard.component';
+import { AddDeviceComponent } from './featured-component/add-device/add-device.component';
+import { AboutComponent } from './featured-component/about/about.component';
+import { DevicesComponent } from './featured-component/devices/devices.component';
+import { UpdateComponent } from './featured-component/update/update.component';
+import { ProfileComponent } from './featured-component/profile/profile.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    DashboardComponent,
+    AddDeviceComponent,
+    AboutComponent,
+    DevicesComponent,
+    UpdateComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
