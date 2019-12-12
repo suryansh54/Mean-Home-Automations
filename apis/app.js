4ddeb15c3f26f5f2a564337de0b72c6ded55b8e4
Suryansh <suryanshsrivastava9616@gmail.com>
2019-12-13 01:46:13 +0530

    Swagget Docs update

--- apis/app.js
+++ apis/app.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocs = require('./swagger/swagger.config');
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-  
+
 
 // For cors 
 app.use(cors());
@@ -34,17 +34,51 @@ app.use('/v1/api', wifi);
 app.use('/v1/api', dynamicCrud);
 
 
-app.listen(port,() => console.log(`Your server is runs into the port ${port}`)); // Creates a server
+app.listen(port, () => console.log(`Your server is runs into the port ${port}`)); // Creates a server
 
 
 
   // Routes
-  /**
-   * @swagger
-   * /v1/api/os:
-   *  get:
-   *    description: Use to request all customers
-   *    responses:
-   *      '200':
-   *        description: A successful response
-   */
+/**
+ * @swagger
+ * /v1/api/os:
+ *  get:
+ *    description: Use to request all customers
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
+
+/**
+* @swagger
+* /v1/api/crud:
+*  get:
+*    description: Use to request all users
+*    responses:
+*      '200':
+*        description: A successful response
+*/
+
+/**
+ * @swagger
+ * /v1/api/crud/<userId>:
+ *  get:
+ *    description: Use to request user by id
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
+
+/**
+* @swagger
+* /v1/api/crud:
+*  post:
+*    summary: Creates a new user.
+*    description: Creates a new user.
+*    parameters:
+*     - in: body
+*       name:
+*    responses:
+*      '200':
+*        description: A successful response
+*/
